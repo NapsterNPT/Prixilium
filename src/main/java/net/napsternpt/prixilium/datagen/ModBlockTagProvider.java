22e7eb0b5dece01d911905aa71976c919f3b64c8
NapsterNPT <188631398+napsternpt@users.noreply.github.com>
2026-02-07 23:48:08 +0100

    removing jsons and making datagen

---
+++ src/main/java/net/napsternpt/prixilium/datagen/ModBlockTagProvider.java
@@ -0,0 +1,46 @@
+package net.napsternpt.prixilium.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+import net.napsternpt.prixilium.block.ModBlocks;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+    public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.AXE_MINEABLE)
+                .add(ModBlocks.PRIXILIUM_LOGS)
+                .add(ModBlocks.PRIXILIUM_PLANKS);
+
+        getOrCreateTagBuilder(BlockTags.HOE_MINEABLE)
+                .add(ModBlocks.PRIXILIUM_LEAVES);
+
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.PRIXILIUM_BRICKS);
+
+        getOrCreateTagBuilder(BlockTags.SHOVEL_MINEABLE)
+                .add(ModBlocks.PRIXILIUM_GRASS);
+
+        getOrCreateTagBuilder(BlockTags.DIRT)
+                .add(ModBlocks.PRIXILIUM_GRASS);
+
+        getOrCreateTagBuilder(BlockTags.LEAVES)
+                .add(ModBlocks.PRIXILIUM_LEAVES);
+
+        getOrCreateTagBuilder(BlockTags.LOGS)
+                .add(ModBlocks.PRIXILIUM_LOGS);
+
+        getOrCreateTagBuilder(BlockTags.PLANKS)
+                .add(ModBlocks.PRIXILIUM_PLANKS);
+
+        getOrCreateTagBuilder(BlockTags.SNAPS_GOAT_HORN)
+                .add(ModBlocks.PRIXILIUM_LOGS);
+    }
+}
---
+++ src/main/java/net/napsternpt/prixilium/datagen/ModLootTableProvider.java
@@ -0,0 +1,4 @@
+package net.napsternpt.prixilium.datagen;
+
+public class ModLootTableProvider {
+}
