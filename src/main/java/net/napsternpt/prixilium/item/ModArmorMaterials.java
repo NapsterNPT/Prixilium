56b6bfdb6637200acb0e27afc57775b787bcc800
NapsterNPT <188631398+napsternpt@users.noreply.github.com>
2026-02-17 16:58:05 +0100

    custom armor texture!!! Allllll

--- src/main/java/net/napsternpt/prixilium/item/ModArmorMaterials.java
+++ src/main/java/net/napsternpt/prixilium/item/ModArmorMaterials.java
@@ -26,8 +26,7 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 1);
                 map.put(ArmorItem.Type.BODY, 3);
             }), 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("leather")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 0, 0));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_leather"))), 0, 0));
 
     public static final RegistryEntry<ArmorMaterial> PRIXILIUM_CHAIN = registerArmorMaterial("prixiled_chainmail",
             () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
@@ -37,8 +36,7 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 2);
                 map.put(ArmorItem.Type.BODY, 4);
             }), 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("chainmail")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 0, 0));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_chainmail"))), 0, 0));
 
     /* 1.21.9+
         public static final RegistryEntry<ArmorMaterial> PRIXILIUM_COPPER = registerArmorMaterial("prixiled_copper",
@@ -49,8 +47,7 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, idk);
                 map.put(ArmorItem.Type.BODY, idk);
             }), idk, SoundEvents.ITEM_ARMOR_EQUIP_COPPER, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("copper")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), idk, idk));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_copper"))), idk, idk));
 
      */
 
@@ -62,8 +59,7 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 2);
                 map.put(ArmorItem.Type.BODY, 5);
             }), 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("iron")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 0, 0));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_iron"))), 0, 0));
 
     public static final RegistryEntry<ArmorMaterial> PRIXILIUM_GOLD = registerArmorMaterial("prixiled_gold",
             () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
@@ -73,8 +69,7 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 2);
                 map.put(ArmorItem.Type.BODY, 7);
             }), 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("gold")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 0, 0));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_gold"))), 0, 0));
 
     public static final RegistryEntry<ArmorMaterial> PRIXILIUM_DIAMOND = registerArmorMaterial("prixiled_diamond",
             () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
@@ -84,8 +79,7 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 3);
                 map.put(ArmorItem.Type.BODY, 11);
             }), 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("diamond")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 2.0F, 0));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_diamond"))), 2.0F, 0));
 
     public static final RegistryEntry<ArmorMaterial> PRIXILIUM_NETHERITE = registerArmorMaterial("prixiled_netherite",
             () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
@@ -95,8 +89,7 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 3);
                 map.put(ArmorItem.Type.BODY, 11);
             }), 10, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("netherite")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 3.0F, 0.1F));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_netherite"))), 3.0F, 0.1F));
 
     public static final RegistryEntry<ArmorMaterial> PRIXILIUM_TURTLE = registerArmorMaterial("prixiled_turtle_scute",
             () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
@@ -106,8 +99,11 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 2);
                 map.put(ArmorItem.Type.BODY, 5);
             }), 9, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, () -> Ingredient.ofItems(ModBlocks.PRIXILIUM),
-                    List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("turtle")),
-                            new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 0, 0));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixiled_turtle_scute"))), 0, 0));
+
+// Overlay
+// List.of(new ArmorMaterial.Layer(Identifier.ofVanilla("meterial")),
+//         new ArmorMaterial.Layer(Identifier.of(Prixilium.MOD_ID, "prixilium"))), 0, 0));
 
     public static RegistryEntry<ArmorMaterial> registerArmorMaterial(String name, Supplier<ArmorMaterial> material) {
         return Registry.registerReference(Registries.ARMOR_MATERIAL, Identifier.of(Prixilium.MOD_ID, name), material.get());
