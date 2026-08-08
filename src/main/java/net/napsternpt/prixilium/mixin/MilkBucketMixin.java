7a160ed251a2636fc555746350855852d00be8cd
NapsterNPT <188631398+napsternpt@users.noreply.github.com>
2026-08-08 15:54:43 +0200

    added prixiled food textures (wiki cooooooming veeeery soon)

--- src/main/java/net/napsternpt/prixilium/mixin/MilkBucketMixin.java
+++ src/main/java/net/napsternpt/prixilium/mixin/MilkBucketMixin.java
@@ -1,22 +1,23 @@
 package net.napsternpt.prixilium.mixin;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.ClearAllEffectsConsumeEffect;
+import net.minecraft.world.World;
 import net.napsternpt.prixilium.effect.ModEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class)
+@Mixin(ClearAllEffectsConsumeEffect.class)
 public class MilkBucketMixin {
 
-	@Inject(method = "clearStatusEffects", at = @At("HEAD"), cancellable = true)
-	private void preventIllnessRemoval(CallbackInfoReturnable<Boolean> cir) {
-		LivingEntity entity = (LivingEntity)(Object)this;
-
+	@Inject(method = "onConsume", at = @At("HEAD"), cancellable = true)
+	private void preventIllnessRemoval(World world, ItemStack stack, LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
 		if (entity.hasStatusEffect(ModEffects.ILLNESS)) {
 			entity.getActiveStatusEffects().entrySet().removeIf(e -> !e.getKey().equals(ModEffects.ILLNESS));
-			cir.setReturnValue(true);
+			cir.setReturnValue(false);
 			cir.cancel();
 		}
 	}
